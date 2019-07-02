b4be6b2e0d6ddd5250aa9ed85048d5dbbbe94418
Martiross99 <martiross99@gmail.com>
2019-07-02 23:27:55 +0200

    lezione1

---
+++ src/model/introSceneLoader.java
@@ -0,0 +1,47 @@
+package model;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class introSceneLoader extends Application{
+
+
+	public static Integer intro = 1;
+	public static String introScene = "/lezione1/1_1.fxml";
+	public static Integer proprieta = 2;
+	public static String proprietaScene = "/lezione1/1_2.fxml";
+	public static Integer esempio = 3;
+	public static String esempioScene = "/lezione1/1_3.fxml";
+	public static Integer gioco = 4;
+	public static String giocoScene = "/lezione1/monete.fxml";
+	public static Integer end = 5;
+	public static String endScene = "/views/1_5.fxml";
+
+	 
+    public Stage stage;
+     
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		sceneController main = new sceneController();
+		main.loadScene(intro, introScene);
+		main.loadScene(proprieta, proprietaScene);
+		main.loadScene(esempio, esempioScene);
+		main.loadScene(gioco, giocoScene);
+		main.loadScene(end, endScene);
+
+		main.setScene(introSceneLoader.intro);
+	        
+		Group root = new Group();
+		root.getChildren().add(main);
+		Scene scene = new Scene(root);
+
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+
+}
