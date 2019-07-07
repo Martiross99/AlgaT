8a49bc922e92a9f5e7cb6a235792f2197844e1a2
erika.lena@outlook.it <erika.lena@outlook.it>
2019-07-07 19:02:21 +0200

    conflitti

--- src/lezione1/navController.java
+++ src/lezione1/navController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package lezione1;
 
 import javafx.event.ActionEvent;
@@ -7,42 +6,6 @@ import javafx.scene.Node;
 import javafx.stage.Stage;
 import model.sceneController;
 //import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-
-import java.io.IOException;
-
-public class navController implements model.ISceneController{
-
-    sceneController sc;
-
-    @FXML
-    void goBack(ActionEvent event) throws IOException {
-        sc.goBack();
-    }
-
-    @FXML
-    void goNext(ActionEvent event) throws IOException {
-        sc.goNext();
-    }
-
-//    @FXML
-//    void goToMenu(ActionEvent event) throws IOException {
-//        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-//        sc.getProgetto().gotoMenu(window);
-//    }
-
-    @Override
-    public void setSceneParent(sceneController sceneParent) {
-        sc = sceneParent;
-    }
-}
-=======
-package lezione1;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.stage.Stage;
-import model.sceneController;
 
 import java.io.IOException;
 
@@ -71,4 +34,3 @@ public class navController implements model.ISceneController{
         sc = sceneParent;
     }
 }
->>>>>>> branch 'master' of https://github.com/erikalena/progettoAlgaT.git
